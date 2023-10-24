1a56f4418c4e1d1e6534f92eea215e25a43ddeec
RenzouM <renzoumgmail.com>
2023-10-23 21:30:08 -0300

    hola

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/asd", (req, res) => {
   res.redirect("http://www.google.com");
 });
 
