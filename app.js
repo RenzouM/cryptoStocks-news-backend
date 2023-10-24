a5e724538146f8186aa3a4356e88c86cc55edb73
RenzouM <renzoumgmail.com>
2023-10-23 21:52:30 -0300

    hola

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.static("public"));
+app.use(cors());
 
 app.get("/news", async (req, res) => {
   try {
